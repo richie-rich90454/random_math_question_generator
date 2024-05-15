d7e8167de7fca3e81d95c541f091239a8746bab2
richie-rich90454 <166470680+richie-rich90454@users.noreply.github.com>
2024-05-15 12:01:14 +0800

    120115

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+let generateBtn=document.getElementById("genQ");
+generateBtn.addEventListener("click",function(){
 
+});
+function genAddQ(){
+    let firstNum=Math.floor(Math.random()*100)+1;
+    let secondNum=Math.floor()
+}
