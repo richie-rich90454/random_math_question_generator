f195046e492d055ea29724d5c98c4ad927bed0c9
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2025-03-15 15:59:11 +0800

    155915 updated css and script

--- script.js
+++
@@ -1,8 +0,0 @@
-let generateBtn=document.getElementById("genQ");
-generateBtn.addEventListener("click",function(){
-
-});
-function genAddQ(){
-    let firstNum=Math.floor(Math.random()*100)+1;
-    let secondNum=Math.floor()
-}
