986cd5d41163245dc0376220671470e1a6e29873
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2025-03-16 07:23:52 +0800

    072316 improved organization

--- script.js
+++ script.js
@@ -2,30 +2,7 @@ let quotesArray=[];
 let authorsArray=[];
 let questionArea=document.getElementById("question-area");
 let questionType=document.getElementById("gen-type")
-document.addEventListener('DOMContentLoaded', ()=>{
-    fetch("/quotes_of_the_day.txt")
-        .then(response=>{
-            if (!response.ok) throw new Error('Network error');
-            return response.text();
-        })
-        .then(data=>{
-            const lines=data.split('\n').map(line=>line.trim()).filter(line=>line.length > 0);
-            lines.forEach((line)=>{
-                const parts=line.split('-');
-                if (parts.length < 2){
-                    return
-                };
-                const quote=parts.slice(0, -1).join('-').trim();
-                const author=parts.slice(-1)[0].trim();
-                quotesArray.push(quote);
-                authorsArray.push(author);
-            });
-            const randomIndex=Math.floor(Math.random()*quotesArray.length);
-            document.getElementById('new-quote').textContent=quotesArray[randomIndex];
-            document.getElementById('author').textContent=`- ${authorsArray[randomIndex]}`;
-        })
-        .catch(error=>console.error('Error:', error));
-});
+let generateQuestionButton=document.getElementById("genQ");
 function generateQuestion(){
     let question=questionType.value;
     switch(question){
@@ -93,3 +70,27 @@ function generateQuestion(){
             questionArea.innerHTML=`Please select a type of question to generate before hitting the "Generate Question" button`
     }
 }
+document.addEventListener('DOMContentLoaded', ()=>{
+    fetch("/quotes_of_the_day.txt")
+        .then(response=>{
+            if (!response.ok) throw new Error('Network error');
+            return response.text();
+        })
+        .then(data=>{
+            const lines=data.split('\n').map(line=>line.trim()).filter(line=>line.length > 0);
+            lines.forEach((line)=>{
+                const parts=line.split('-');
+                if (parts.length < 2){
+                    return
+                };
+                const quote=parts.slice(0, -1).join('-').trim();
+                const author=parts.slice(-1)[0].trim();
+                quotesArray.push(quote);
+                authorsArray.push(author);
+            });
+            const randomIndex=Math.floor(Math.random()*quotesArray.length);
+            document.getElementById('new-quote').textContent=quotesArray[randomIndex];
+            document.getElementById('author').textContent=`- ${authorsArray[randomIndex]}`;
+        })
+        .catch(error=>console.error('Error:', error));
+});
